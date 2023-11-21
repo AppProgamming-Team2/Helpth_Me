1a626d7a0067bb0aeffcbe5e7c44fa3dcc14f7c8
hyunwoo0320 <hw030320@gmail.com>
2023-11-21 22:05:04 +0900

    파일명 수정, member.js 오류 수정

--- backend_member/login.php
+++ backend_member/login.php
@@ -1,24 +1,45 @@
 <!DOCTYPE html>
-<html lang="ko">
+<html>
 <head>
-  <meta charset="utf-8">
-  <title>로그인</title>
+   <meta charset="utf-8">
+   <title></title>
 </head>
 <body>
-  <form method="post" action="check_login.php" class="loginForm">
-    <h2>로그인</h2>
-    <div class="idForm">
-      <input type="text" name="id" class="id" placeholder="Username">
-    </div>
-    <div class="passForm">
-      <input type="password" name="pw" class="pw" placeholder="Password">
-    </div>
-    <button type="submit" class="btn" onclick="button()">
-      로그인
-    </button>
-    <div class="bottomText">
-      <a href="join/join_page.html">회원가입</a>
-    </div>
-  </form>
-</body>
-</html>
+   <?php
+   session_start();
+   include "db.php";
+      
+      //login.php에서 입력받은 id, password
+      $email = $_POST['id'];
+      $password = $_POST['pw'];
+       
+      //동일한 이름으로 저장된 id, password를 select함 
+      $sql = "SELECT email, password FROM tb1 WHERE email='$email' AND
+      password='$password'";
+
+      //mysqli_query : 서버에 접속한 뒤($mysqli) 테이블의 값들을 모두 표현
+      $result = mysqli_query($mysqli, $sql);
+
+      //fetch_array : array 형태로 가져옴, 넘어온 결과를 한 행씩 패치해서 $row라는 배열에 담음. 함수의 인자로 들어갈 수 있는 값은 3개(NUM, ASSOC, BOTH)
+
+      //NUM : 숫자 첨자를 통해 데이터를 호출
+      //ASSOC : 연관배열의 약자, 필드명(열이름,키값)을 통해 데이터를 호출
+      //BOTH : 둘다 가능
+      $row = $result->fetch_array(MYSQLI_ASSOC);
+      
+      //결과가 존재하면 세션 생성
+      if ($row != null) {
+         $_SESSION['username'] = $row['email'];
+         $_SESSION['name'] = $row['name'];
+         echo "<script>location.replace('index.php');</script>";
+         exit;
+      }
+      
+      //결과가 존재하지 않으면 로그인 실패
+      if($row == null){
+         echo "<script>alert('ID 또는 비밀번호가 틀립니다')</script>";
+         echo "<script>location.replace('login.php');</script>";
+         exit;
+      }
+      ?>
+   </body>
--- backend_member/member.js
+++ backend_member/member.js
@@ -9,7 +9,7 @@
       function id_duplication() {
       /* ID 중복 확인 버튼을 클릭했을 때 실행할 코드 */
       /* PHP 파일로부터 받은 응답을 처리하는 코드*/
-      var email = document.getElementById("id").value;
+      var id = document.getElementById("id").value;
       $.ajax({
         url: 'check_duplicate.php', // PHP 파일의 경로 
         type: 'POST',
@@ -29,9 +29,9 @@
 
       function receive() { 
       /* PHP 파일로부터 받은 응답을 처리하는 코드*/ 
-      var password= document.getElementById("pw").value; 
-      var password_confirm = document.getElementById("pw_cf").value; 
-      var ID_confirm = duplicate_message;
+      password= document.getElementById("pw").value; 
+      password_confirm = document.getElementById("pw_cf").value; 
+      ID_confirm = duplicate_message;
       }
 
 
@@ -40,7 +40,7 @@
        receive()
 
       // 특정 조건 확인
-      if (password === " "){ 
+      if (password === "" | password_confirm === ""){ 
         $('#password_confirm_error').html("패스워드를 입력해주세요") 
 
         }
@@ -49,19 +49,20 @@
 
 
         if (password === password_confirm) { 
-          $('#password_confirm_error').html ("패스워드가 일치합니다.")
-          if(duplicate_message==="사용 가능한 ID입니다.") { 
-            return true;
-          } 
-          else {
-            console.log(ID_confirm); 
-            $('#confirm_error').html ("ID를 다시 확인해주세요.");
-            return false; }
-        } 
-        else {
-        // 조건 미충족 시 페이지 이동 중지 
-        $('#password_confirm_error').html("패스워드가 일치하지 않습니다.");
-        return false;
+            $('#password_confirm_error').html ("패스워드가 일치합니다.")
+            if(duplicate_message==="사용 가능한 ID입니다.") { 
+              return true;
+            } 
+            else {
+              console.log(ID_confirm); 
+              $('#confirm_error').html ("ID를 다시 확인해주세요.");
+              return false; 
+            }
+
+        } else {
+          // 조건 미충족 시 페이지 이동 중지 
+          $('#password_confirm_error').html("패스워드가 일치하지 않습니다.");
+          return false;
         }
 
       }
